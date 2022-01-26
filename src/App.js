3bcc3c329ae2cf03c52b013cd17c45be32831820
nyopko <44181737+nyopko@users.noreply.github.com>
2022-01-25 20:52:35 -0500

    Added React Routing, set up basic page skeletons.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+/// Components import
+import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+
+/// Stylesheet import 
+
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+/// Pages import
+
+import Home from "./Pages/home.js";
+import Profile from "./Pages/profile.js";
+import Leaderboard from "./Pages/leaderboard.js";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/leaderboard" component={Leaderboard} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/home.js
@@ -0,0 +1,16 @@
+/// Components import
+import React, { Component } from 'react';
+
+
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <h1>Home</h1>
+        );
+    }
+}
+
+export default App;
